825764cec10e9cd0e146c1dc7f6ae1e9d93040b1
Michael Chen <j.michael.chen@gmail.com>
2022-12-01 18:06:34 -0700

    Forgot a file

---
+++ ssh_to_device.py
@@ -0,0 +1,71 @@
+import paramiko
+
+class ssh_to_device:
+    """
+    Class for defining connections to the device via SSH.
+    Attributes:
+        client: The SSH client as defined by the Paramiko library.
+        connected: A boolean describing whether a connection has been opened.
+    """
+    def __init__(self):
+        self.client = None
+        self.connected = False
+
+    def connect(self, IP_address, port=22):
+        try:
+            self.disconnect()
+        finally:
+            pass
+        self.client = paramiko.SSHClient()
+        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            self.client.connect(hostname=IP_address,username='root',password='')
+            self.connected = True
+            return True
+        except Exception as e:
+            print("Unable to establish SSH connection to " + IP_address)
+            print(f"Reason: {e}")
+            return False
+
+    def disconnect(self):
+        if self.client:
+            self.client.close()
+        self.connected = False
+
+    def execute(self, command):
+        if not self.client:
+            print("Client is not connected. Need to connect first.")
+            return None
+        stdin, stdout, stderr = self.client.exec_command(command)
+        return stdout
+
+    def collect_data(self, firmware_file, file_name = ""):
+        resp = self.execute(f"sudo python3 {firmware_file} {file_name}")
+
+    def upload_firmware(self, local_firmware, remote_firmware):
+        if not self.client:
+            print("Client is not connected. Need to connect first.")
+            return False
+        try:
+            ftp_client = self.client.open_sftp()
+            ftp_client.put(local_firmware, remote_firmware)
+            ftp_client.close()
+            return True
+        except Exception as e:
+            print("Could not upload firmware to device")
+            print(f"Reason: {e}")
+            return False
+
+    def download_file(self, file, dest):
+        if not self.client:
+            print("Client is not connected. Need to connect first.")
+            return False
+        try:
+            ftp_client = self.client.open_sftp()
+            ftp_client.get(file, dest)
+            ftp_client.close()
+            return True
+        except Exception as e:
+            print(f"Could not download file {file} to device")
+            print(f"Reason: {e}")
+            return False
