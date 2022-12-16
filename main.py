aa7535c3440255df6b519ae476f52dcd56aad983
Michael Chen <j.michael.chen@gmail.com>
2022-12-15 17:17:09 -0800

    Updated todo list

--- main.py
+++ main.py
@@ -8,27 +8,30 @@ def main():
 if __name__ == "__main__":
 	main()
 
-"""
-to discuss
-	future maintenance of this project: github or google drive?
-	would it be ok to refactor the code so that it essentially calls the existing mcp_driver code (which requires you to press buttons on the microcontroller)? this would avoid having to decode the mcp_driver code which has many mysterious definitions
-		two paths:
-			on the GUI, press the "baseline" or "sample" buttons. this initiates a script on the microcontroller to gather data and send the data file back to the user's computer
-			on the GUI, press the "baseline" or "sample" buttons. this displays messages similar to those on the ssh_to_covid_board program, instructing the user to place samples in the microcontroller, then gathering baseline/sample and sending data files to the user
-"""
 
-# currently hard codes the ip addresses; the old program currently selects the first detected ip address; eventually will implement a drop down with the detected list of ip addresses and you choose one then click connect
-# add axes to the graphs!!!
-# driver program does not exit after generating raw data file so it needs to be remotely terminated from the gui program
-# enhancement: export json of the qc parameters
-# enhancement: add title text to the plots
+# next meeting after 12/15 is on 1/5 4pm
 
-# put some feedback to the terminal when and after connecting successfully
-# imports for microcontroller: openpyxl
+
+# HIGH PRIORITY
+# ENHANCEMENT: imports for microcontroller: openpyxl
 #   new devices to come soon
-# data picked was the last sweep when it should have been a "middle" sweep
+# BUG: data picked was the last sweep when it should have been a "middle" sweep when running analytics (qc/dirac)
+# ENHANCEMENT: feedback while waiting for file when clicking baseline or sample buttons
+# ENHANCEMENT: begin testing compiling the program into a single executable
 
-# show score from q/c in the gui
-# kill process when finished getting csv file
-# remove csv file from remote after downloading
-# sampling file did not get retrieved
+# Medium priority
+# ENHANCEMENT: implement a drop down with the detected list of ip addresses and you choose one then click connect
+#   BUG: the first time "connect" is clicked, the IP address field gets wiped
+# BUG: firmware program does not exit after generating raw data file so it needs to be remotely terminated from the gui program
+#   solution 1: the GUI kills the process after starting it (using ps | grep maybe) DO THIS ONE
+#   solution 2: write a standalone script that kills all running processes on the microcontroller
+#   when running firmware program manually by SSHing into device, need to CTRL+C twice to exit
+# ENHANCEMENT: remove csv file from remote after downloading
+
+# Low priority
+# ENHANCEMENT: add axes to graphs
+# ENHANCEMENT: add title text to graphs
+# ENHANCEMENT: put some feedback to the terminal when and after connecting successfully
+# ENHANCEMENT: notify the user if the ssh connection breaks any time
+# ENHANCEMENT: rename "samplingThread" to "data collection" etc. (NOT 'sampling' from BASELINE/SAMPLING)
+# ENHANCEMENT: when writing results with results button, add filenames for both baseline and sample files used into the JSON output
