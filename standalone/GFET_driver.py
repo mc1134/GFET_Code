77aca3e3391fcc7ec6a68fa28cb4689f85cd1902
Michael Chen <j.michael.chen@gmail.com>
2023-02-28 13:14:02 -0700

    Added splitz method

--- standalone/GFET_driver.py
+++ standalone/GFET_driver.py
@@ -30,6 +30,7 @@ import json
 from os import path
 import socket
 import traceback
+import ruptures as rpt
 
 USE_FAKE_DATA = False # For generating fake Gate Voltages / Ids for testing without sensor
 
@@ -1414,13 +1415,69 @@ class LED_thread(threading.Thread):
 def get_time():
     return time.strftime("%Y-%m-%dT%H-%M-%SZ%z")
 
+def splitz_new_opt(data):
+    """data assumed to possess the following structure:
+    [
+        ['Gate Voltage', 'Ids'],
+        ['-0.12345', '6.789e-05'],
+        ['0.2468', '-1.3579e-02'],
+        ...
+    ]
+    """
+    forw = []
+    back = []
+    """both arrays will represent the original 3D matrix with the following structure:
+    [
+        [
+            [gate_voltages_1, ids_1],
+            ['-0.1234', '6.78e-05'],
+            ...
+        ],
+        [
+            [gate_voltages_2, ids_2],
+            ['0.2468', '-1.3579e-02'],
+            ...
+        ],
+        ...
+    ]
+    """
+
+    # Finding where the voltage changes direction in the sweep + to - viceversa
+    V = [float(d[0]) for d in data[1:]] # casts [[header1,header2],[a1,a2],[b1,b2],[c1,c2],...] list of str lists into [a1,b1,c1,...] list of floats
+    dv = np.diff(V)
+
+    # Number of splits calculation
+    numsweep = 793 # Number of datapoints per sweep
+    swns = int(len(dv)/numsweep) # Number of sweeps roughly
+
+    pts = rpt.Binseg(model = "l2").fit(dv).predict(n_bkps = swns) # find change points. this takes a very long time (5 minutes for 700 data points?)
+
+    # Splitting the forward and backward into two separate arrays
+    sam = np.mean(dv[pts[1]:pts[2]])
+    if sam > 0:
+        pidx = 1 # Cycle through the pts array
+        for val in range(1,(len(pts)-1)//2):
+            forw += [data[pts[pidx]  +20:pts[pidx]  +numsweep-20]]
+            back += [data[pts[pidx+1]+20:pts[pidx+1]+numsweep-20]]
+            pidx += 2
+    else:
+        pidx = 1
+        for val in range(1, (len(pts)-1)//2):
+            back += [data[pts[pidx]  +20:pts[pidx]  +numsweep-20]]
+            forw += [data[pts[pidx+1]+20:pts[pidx+1]+numsweep-20]]
+            pidx += 2
+
+    return forw, back
+
 def quality_control(baseline_data, sampling_data, sweep_num):
-    bx = [obj[0] for obj in baseline_data[sweep_num]] # voltages
-    by = [obj[1] for obj in baseline_data[sweep_num]] # ids
+    baseline_fx, baseline_bx = splitz_new_opt(baseline_data)
+    sampling_fx, sampling_bx = splitz_new_opt(sampling_data)
+    bx = [obj[0] for obj in baseline_fx[sweep_num]] # voltages
+    by = [obj[1] for obj in baseline_fx[sweep_num]] # ids
     bxn = [item / max(bx) for item in bx] # normalized x
     byn = [item / max(by) for item in by] # normalized y
-    sx = [obj[0] for obj in sampling_data[sweep_num]]
-    sy = [obj[1] for obj in sampling_data[sweep_num]]
+    sx = [obj[0] for obj in sampling_fx[sweep_num]]
+    sy = [obj[1] for obj in sampling_fx[sweep_num]]
     sxn = [item / max(sx) for item in sx]
     syn = [item / max(sy) for item in sy]
 
